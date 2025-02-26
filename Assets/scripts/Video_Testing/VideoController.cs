56edc67c2c194b56a6e08e68c341b26f1fc9a88d
SaiHarish820 <2100031942@kluniversity.in>
2025-02-26 18:14:17 +0530

    Video Player Done, should work on seekbar

---
+++ Assets/scripts/Video_Testing/VideoController.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Button playPauseButton;
+    public Sprite playSprite;
+    public Sprite pauseSprite;
+    public Slider seekBar;
+    public Image clickableImage; // Image with collider to show/hide UI
+
+    [SerializeField] private bool isDragging = false;
+    private float uiVisibleTimer = 0f;
+    private bool isUIVisible = false;
+
+    void Start()
+    {
+        // Initialize UI visibility
+        SetUIVisibility(false);
+
+        // Add listeners
+        playPauseButton.onClick.AddListener(TogglePlayPause);
+        seekBar.onValueChanged.AddListener(OnSeekBarValueChanged);
+        videoPlayer.loopPointReached += OnVideoEnd;
+
+        // Add click listener to the image
+        EventTrigger trigger = clickableImage.gameObject.AddComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener((data) => OnImageClick());
+        trigger.triggers.Add(entry);
+
+        // Set seek bar range
+        seekBar.minValue = 0;
+        seekBar.maxValue = 1;
+    }
+
+    void Update()
+    {
+        // Update seek bar if not dragging
+        if (!isDragging && videoPlayer.length > 0)
+        {
+            seekBar.value = (float)(videoPlayer.time / videoPlayer.length);
+        }
+
+        // Update button sprite
+        UpdateButtonSprite();
+
+        // Handle UI visibility timer
+        if (isUIVisible)
+        {
+            uiVisibleTimer -= Time.deltaTime;
+            if (uiVisibleTimer <= 0)
+            {
+                SetUIVisibility(false);
+            }
+        }
+    }
+
+    void OnImageClick()
+    {
+        // Show UI and reset the timer
+        SetUIVisibility(true);
+        uiVisibleTimer = 5f; // 5 seconds
+    }
+
+    void SetUIVisibility(bool visible)
+    {
+        isUIVisible = visible;
+        playPauseButton.gameObject.SetActive(visible);
+        seekBar.gameObject.SetActive(visible);
+    }
+
+    void TogglePlayPause()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        }
+        else
+        {
+            videoPlayer.Play();
+        }
+        UpdateButtonSprite();
+    }
+
+    void UpdateButtonSprite()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            playPauseButton.image.sprite = pauseSprite;
+        }
+        else
+        {
+            playPauseButton.image.sprite = playSprite;
+        }
+    }
+
+    void OnSeekBarValueChanged(float value)
+    {
+        if (isDragging && videoPlayer.length > 0)
+        {
+            videoPlayer.time = value * videoPlayer.length;
+        }
+    }
+
+    public void OnPointerDown()
+    {
+        isDragging = true;
+        videoPlayer.Pause();
+    }
+
+    public void OnPointerUp()
+    {
+        isDragging = false;
+        videoPlayer.time = seekBar.value * videoPlayer.length;
+        videoPlayer.Play();
+        UpdateButtonSprite();
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        seekBar.value = 0;
+        UpdateButtonSprite();
+    }
+}
